37298ae0435f3cb694859094b5c8b8867ff69ef2
Luis Corales <luisaugustocorales@hotmail.com>
2022-12-08 22:07:19 -0500

    Update decorator.js

--- structural/decorator.js
+++ structural/decorator.js
@@ -1,67 +1,112 @@
-import { useState } from 'react'
-import Context from './Context'
+import { useState, useContext } from "react";
+import Context from "./Context";
 
-const ContextProvider = ({children}) => {
+const ContextProvider = ({ children }) => {
+  const [darkModeOn, setDarkModeOn] = useState(true);
+  const [englishLanguage, setEnglishLanguage] = useState(true);
 
-    const [darkModeOn, setDarkModeOn] = useState(true)
-    const [englishLanguage, setEnglishLanguage] = useState(true)
-
-    return (
-        <Context.Provider value={{
-            darkModeOn,
-            setDarkModeOn,
-            englishLanguage,
-            setEnglishLanguage
-        }} >
-            {children}
-        </Context.Provider>
-    )
-}
-
+  return (
+    <Context.Provider
+      value={{
+        darkModeOn,
+        setDarkModeOn,
+        englishLanguage,
+        setEnglishLanguage,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
 
 export default function App() {
   return (
     <ContextProvider>
       <Router>
-
-        <ErrorBoundary>
-          <Suspense fallback={<></>}>
-            <Header />
-          </Suspense>
-
-          <Routes>
-              <Route path='/' element={<Suspense fallback={<></>}><AboutPage /></Suspense>}/>
-
-              <Route path='/projects' element={<Suspense fallback={<></>}><ProjectsPage /></Suspense>}/>
-
-              <Route path='/projects/helpr' element={<Suspense fallback={<></>}><HelprProject /></Suspense>}/>
-
-              <Route path='/projects/myWebsite' element={<Suspense fallback={<></>}><MyWebsiteProject /></Suspense>}/>
-
-              <Route path='/projects/mixr' element={<Suspense fallback={<></>}><MixrProject /></Suspense>}/>
-
-              <Route path='/projects/shortr' element={<Suspense fallback={<></>}><ShortrProject /></Suspense>}/>
-
-              <Route path='/curriculum' element={<Suspense fallback={<></>}><CurriculumPage /></Suspense>}/>
-
-              <Route path='/blog' element={<Suspense fallback={<></>}><BlogPage /></Suspense>}/>
-
-              <Route path='/contact' element={<Suspense fallback={<></>}><ContactPage /></Suspense>}/>
-          </Routes>
-        </ErrorBoundary>
-
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<></>}>
+                <AboutPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <Suspense fallback={<></>}>
+                <ProjectsPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/projects/helpr"
+            element={
+              <Suspense fallback={<></>}>
+                <HelprProject />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/projects/myWebsite"
+            element={
+              <Suspense fallback={<></>}>
+                <MyWebsiteProject />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/projects/mixr"
+            element={
+              <Suspense fallback={<></>}>
+                <MixrProject />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/projects/shortr"
+            element={
+              <Suspense fallback={<></>}>
+                <ShortrProject />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/curriculum"
+            element={
+              <Suspense fallback={<></>}>
+                <CurriculumPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/blog"
+            element={
+              <Suspense fallback={<></>}>
+                <BlogPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <Suspense fallback={<></>}>
+                <ContactPage />
+              </Suspense>
+            }
+          />
+        </Routes>
       </Router>
     </ContextProvider>
-  )
+  );
 }
-
-
 
 const AboutPage = () => {
+  const { darkModeOn } = useContext(Context);
 
-    const { darkModeOn, englishLanguage } = useContext(Context)
-    
-    return <></>
-}
+  const [darkMode, setDarkMode] = useState(darkModeOn);
+  return <></>;
+};
 
-export {AboutPage, ContextProvider}
+export { AboutPage, ContextProvider };
